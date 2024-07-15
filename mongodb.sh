660ca2a2462cc99e4923da1b220b0d81412196b8
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-15 13:56:15 +0530

    mongo.repo file commited

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
+ID=$(id -u)
 
-echo "pracrlkkh"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+TIME_STAMP=$(date +%F:%H:%M:%S)
+LOG_FILE=/tmp/$0_$TIME_STAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ...$R Failed $N"
+        exit 1
+    else
+        echo -e "$2 ....$G Success $N"
+  fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error:: Please run this script with root access"
+    exit 1
+else
+    echo "$G you are root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
+VALIDATE $? "Cpoied mongo.repo"
