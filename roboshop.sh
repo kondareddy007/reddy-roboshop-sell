85167f886960b19e8095dcce40bd9bb06ea1db77
kondareddy1229 <kondareddy.devops@gmail.com>
2024-09-12 00:22:25 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -2,12 +2,12 @@
 
 IMAGE_ID=ami-0182f373e66f89c85
 SG_ID=sg-06b8a8118fee179ad
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCES=( "mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web" )
 
 for i in "${INSTANCES[@]}"
 do
 echo "instances is: $i"
-if [ $i == "mongodb" ] || [ $i == "mysql"] || [ $i == "shipping"]
+if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
 then
      INSTANCE_TYPE="t3.small"
 else
